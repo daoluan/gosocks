b3181975589b48c6083f736b00b61aef16c75da6
dylanzheng <g.daoluan@gmail.com>
2018-07-21 15:17:31 +0800

    屏幕日志该用文件日志；服务端问题修

--- gssrv/client_proxy.go
+++ gssrv/client_proxy.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"github.com/daoluan/gosocks/common"
+	"log"
 	"net"
+
+	"github.com/daoluan/gosocks/common"
 )
 
 type ClientProxy struct {
@@ -15,19 +16,24 @@ type ClientProxy struct {
 }
 
 func (c *ClientProxy) Init(src_conn net.Conn) {
-	c.src_conn = nil
+	c.src_conn = src_conn
 	c.dst_conn = nil
 	c.reqlen = 0
 	c.state = -1
+	c.reqbuf = make([]byte, 100000)
 }
 
 func (c *ClientProxy) Feed() bool {
 	if c.src_conn == nil {
+		log.Println("srconn nil")
 		return false
 	}
 
+	log.Println("reading from localproxy c.reqlen", c.reqlen, len(c.reqbuf))
 	reqlen, err := c.src_conn.Read(c.reqbuf[c.reqlen:])
+	log.Println("read from localproxy reqlen", reqlen, len(c.reqbuf))
 	if err != nil {
+		log.Println("haha error", err.Error())
 		return false
 	}
 
@@ -40,50 +46,66 @@ func (c *ClientProxy) HandleAuth() bool {
 	_, content, _, err := common.UnpackPacket(c.reqbuf[:c.reqlen])
 	encrpt, _ := common.DecryptDES(content)
 	domain := string(encrpt)
+	log.Println(domain)
 	tcp_addr, err := net.ResolveTCPAddr("tcp4", domain)
 	if err != nil {
-		fmt.Println(domain, "ResolveTCPAddr error: ", err.Error(), c.reqlen)
+		log.Println(domain, "ResolveTCPAddr error: ", err.Error(), c.reqlen)
 		return false
 	}
 
 	c.dst_conn, err = net.DialTCP("tcp4", nil, tcp_addr)
 	if err != nil {
-		fmt.Println(domain, "DialTcp error: ", err.Error(), c.reqlen)
+		log.Println(domain, "DialTcp error: ", err.Error(), c.reqlen)
+		c.dst_conn = nil
 		return false
 	}
-	c.src_conn.Write([]byte{1})
+	// inorder to notify succeed connect dst server
+	c.src_conn.Write([]byte{9})
+
 	// handle proxy to client
-	go doProxyRequest(c.src_conn, c.dst_conn)
+	log.Println("proxy request")
 	c.state = 1
+	c.reqlen = 0
 	return true
 }
 
 func (c *ClientProxy) HandleProxy() bool {
 	for {
 		cmd, content, left, err := common.UnpackPacket(c.reqbuf[:c.reqlen])
+
 		if err != nil || cmd != 2 {
 			if err != nil {
-				fmt.Println("unpack packet error: ringbuf_len = ", c.reqlen,
+				log.Println("unpack packet error: ringbuf_len = ", c.reqlen,
 					", content len = [", c.reqbuf[1:5], "]",
 					", cmd = ", c.reqbuf[0:2],
 					", reqlen = ", c.reqlen)
 			} else if cmd != 2 {
-				fmt.Println("error pkg, cmd: ", cmd, "error: ", err)
+				log.Println("error pkg, cmd: ", cmd, "error: ", err)
+			}
+
+			if err != nil && cmd == 0 {
+				return true
 			}
 			return false
 		}
 
-		c.reqbuf = left
+		c.reqbuf = make([]byte, 100000)
+		copy(c.reqbuf[0:], left)
 		c.reqlen = len(left)
 
 		// handle the complete package
 		plaintext, _ := common.DecryptDES(content)
 		c.dst_conn.Write(plaintext)
+		log.Println("<<< write plaintext to webserver len(plaintext)=", len(plaintext), "reqlen=", c.reqlen,
+			len(c.reqbuf))
 
-		if cmd == 2 && c.reqlen > 0 {
-			continue
+		if c.reqlen == 0 {
+			break
 		}
-		break
+		// if cmd == 2 && c.reqlen > 0 {
+		// 	continue
+		// }
+		// break
 	}
 	return true
 }
@@ -93,6 +115,7 @@ func (c *ClientProxy) GetState() int8 {
 }
 
 func (c *ClientProxy) Fini() {
+	log.Println("fini")
 	if c.src_conn != nil {
 		c.src_conn.Close()
 	}
@@ -106,19 +129,22 @@ func doProxyRequest(src_conn net.Conn, dst_conn net.Conn) {
 	defer dst_conn.Close()
 
 	for {
-		buf := make([]byte, 15120)
-		reqlen, err := dst_conn.Read(buf)
+		buf := make([]byte, 151200)
+		log.Println("reading from webserver")
+		reqlen, err := dst_conn.Read(buf[0:])
+		log.Println("read from webserver read return", "reqlen=", reqlen)
 		if err != nil {
-			fmt.Println("read from webserver error: ", err.Error())
+			log.Println("read from webserver error: ", err.Error())
 			break
 		}
 		if reqlen == 0 {
 			break
 		}
 
+		log.Println("yindan ", buf[0:10])
 		ciphertext, _ := common.EncrptDES(buf[:reqlen])
 		sendbuf := common.PackPacket(2, ciphertext)
-		fmt.Println("recv len = ", reqlen,
+		log.Println(">>> recv len = ", reqlen,
 			", send to client: ", len(sendbuf),
 			", encrpt content len = ", len(ciphertext))
 		src_conn.Write(sendbuf)
--- gssrv/main.go
+++ gssrv/main.go
@@ -1,33 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net"
 	"os"
+
+	"github.com/daoluan/gosocks/common"
 )
 
 const (
-	// CONN_HOST = "0.0.0.0"
-	CONN_HOST = "127.0.0.1"
-	CONN_PORT = "5557"
-	CONN_TYPE = "tcp"
+	CONN_HOST = "0.0.0.0"
+	// CONN_HOST = "127.0.0.1"
+	CONN_PORT = "8080"
+	CONN_TYPE = "tcp4"
 )
 
 func main() {
+	common.InitLog()
 	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
 	if err != nil {
-		fmt.Println("error listening:", err.Error())
+		log.Println("error listening:", err.Error())
 		os.Exit(1)
 	}
 	defer l.Close()
 
-	fmt.Println("listening on " + CONN_HOST + ":" + CONN_PORT)
+	log.Println("listening on " + CONN_HOST + ":" + CONN_PORT)
 
 	for {
 		// listen for an incoming connection
 		conn, err := l.Accept()
 		if err != nil {
-			fmt.Println("Error accepting: ", err.Error())
+			log.Println("Error accepting: ", err.Error())
 			os.Exit(1)
 		}
 
@@ -36,21 +39,28 @@ func main() {
 }
 
 func handleRequest(src_conn net.Conn) {
+	log.Println("new request")
 	var cp ClientProxy
 	cp.Init(src_conn)
 	defer cp.Fini()
 
 	for {
+		log.Println("state", cp.state)
 		if !cp.Feed() {
+			log.Println("feed error")
 			break
 		}
 
 		if cp.GetState() < 0 {
 			if !cp.HandleAuth() {
+				log.Println("auth error")
+
 				break
 			}
+			go doProxyRequest(cp.src_conn, cp.dst_conn)
 		} else if cp.GetState() > 0 {
 			if !cp.HandleProxy() {
+				log.Println("HandleProxy error")
 				break
 			}
 		}
